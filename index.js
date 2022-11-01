fca0f070ca0416473b473c652634d53b1778619f
Morgan VanYperen <morgvanny@gmail.com>
2022-11-01 15:54:16 -0700

    commit tree c54770b7aba25b177cf57a4d964f5764a91160a2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(games) {
+  const wonGame = games.find((game) => {
+    return game.result === "W";
+  });
+  if (wonGame) {
+    return wonGame.year;
+  }
+}
